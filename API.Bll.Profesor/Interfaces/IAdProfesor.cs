95924e370ac9b0c39a583e2d2c4d1de37c2ee369
davila06 <davila06@gmail.com>
2022-07-04 22:29:02 -0600

    Sede + Carrera

--- API.Bll.Profesor/Interfaces/IAdProfesor.cs
+++ API.Bll.Profesor/Interfaces/IAdProfesor.cs
@@ -9,12 +9,10 @@ namespace API.Bll.Profesor.Interfaces
     public interface IAdProfesor
     {
         API.Dto.Profesor.Salida.VerTodosProfesores VerTodosProfesores();
-
         API.Dto.Profesor.Salida.VerDetalleProfesor VerDetalleProfesor(API.Dto.Profesor.Entrada.VerDetalleProfesor pInformacion);
         API.Dto.Profesor.Salida.AgregarProfesor AgregarProfesor(API.Dto.Profesor.Entrada.AgregarProfesor pInformacion);
         API.Dto.Profesor.Salida.EditarProfesor EditarProfesor(API.Dto.Profesor.Entrada.EditarProfesor pInformacion);
         API.Dto.Profesor.Salida.EliminarProfesor EliminarProfesor(API.Dto.Profesor.Entrada.EliminarProfesor pInformacion);
-
 
     }
 }
